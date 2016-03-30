59e8d244ebab27c0b05adbfc02e8d5cc01c0e9af
hes <heshengyx@qq.com>
2016-03-30 19:09:15 +0800

    3

--- src/test/java/com/house/agency/TestTrade.java
+++ src/test/java/com/house/agency/TestTrade.java
@@ -1,5 +1,7 @@
 package com.house.agency;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,6 +21,7 @@ public class TestTrade extends BaseJunitTest {
 		param.setPrice(500000);
 		param.setType("2");
 		param.setTitle("梅林一村三房98平 景观无敌 看房方便 低于市场价");
+		param.setReleaseTime(new Date());
 		tradeService.save(param);
 	}
 }
